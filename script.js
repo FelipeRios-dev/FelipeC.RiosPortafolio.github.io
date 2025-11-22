9552886edb0247b0db0672b6f970b0b4647d6fe6
FelipeRios-Dev <juancastrorios18@gmail.com>
2025-11-22 17:32:17 -0500

    Js y cambio de foto

---
+++ script.js
@@ -0,0 +1,7 @@
+// Cuando se hace clic en el botón, se abre el WhatsApp
+document.getElementById("btnWp").addEventListener("click", () => {
+    window.open(
+        "https://wa.me/3122114428?text=Hola%20Felipe,%20vengo%20de%20tu%20página%20web%20y%20quiero%20tus%20servicios.",
+        "_blank"
+    );
+});
